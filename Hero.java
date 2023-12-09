73cc2a0d6d54efcded651b573896a25ac9679e2a
Cory Drangel <83146790+cjdrangel209@users.noreply.github.com>
2023-12-09 08:59:13 -0500

    Comments for Hero.java

--- Hero.java
+++ Hero.java
@@ -1,9 +1,10 @@
 public class Hero {
-    private int height;
-    private int width;
-    private int x;
-    private int y;
+    private int height;    //variable for the height of the hero
+    private int width;     //variable for the width of the hero
+    private int x;        //variable for x position of hero
+    private int y;        //variable for y position of hero
 
+    //Hero constructor
     public Hero(int h, int w, int x, int y){
         this.height = h;
         this.width = w;
@@ -11,34 +12,42 @@ public class Hero {
         this.y = y;
     }
 
+    //sets the height of hero
     public void setHeight(int h){
         this.height = h;
     }
 
+    //gets the height of hero
     public int getHeight(){
         return height;
     }
 
+    //sets the width of hero
     public void setWidth(int w){
         this.width = w;
     }
 
+    //gets the width of hero
     public int getWidth(){
         return width;
     }
 
+    //sets x position of hero
     public void setX(int x){
         this.x = x;
     }
 
+    //gets x position of hero
     public int getX(){
         return x;
     }
 
+    //sets y position of hero
     public void setY(int y){
         this.y = y;
     }
 
+    //gets y position of hero
     public int getY(){
         return y;
     }
